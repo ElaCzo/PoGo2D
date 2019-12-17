d35c7b876f361fc993c59b222023c5e94376beac
ElisabethAbbasZadeh <elisabeth.abbaszadeh@gmail.com>
2019-12-17 11:20:27 +0100

    WIP collection de pokémon.

--- app/src/main/java/com/example/pogo2d/CollectionActivity.java
+++ app/src/main/java/com/example/pogo2d/CollectionActivity.java
@@ -7,7 +7,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -22,6 +28,18 @@ import java.util.Map;
 public class CollectionActivity extends AppCompatActivity {
 
     private String TAG = "collection";
+
+    private static final String[] numbers = new String[] {
+            "A", "B", "C", "D", "E",
+            "F", "G", "H", "I", "J",
+            "K", "L", "M", "N", "O",
+            "P", "Q", "R", "S", "T",
+            "U", "V", "W", "X", "Y", "Z"};
+
+    private ImageView pokemonImg;
+    private static final Integer[] items = { R.drawable.pikachu,
+            R.drawable.pikachu, R.drawable.pikachu, R.drawable.pikachu, R.drawable.pikachu,
+            R.drawable.pikachu, R.drawable.pikachu, R.drawable.pikachu, R.drawable.pikachu};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +57,24 @@ public class CollectionActivity extends AppCompatActivity {
                 startActivity(intent);    //Afficher la vue
             }
         });*/
+
+        GridView gridView = (GridView) findViewById(R.id.gridView);
+        pokemonImg = (ImageView) findViewById(R.id.img);
+
+        gridView.setAdapter(new CustomGridAdapter(this, items));
+        grid.setOnItemClickListener(this);
+        ArrayAdapter<Integer> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, items);
+
+        gridView.setAdapter(adapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(getApplicationContext(),
+                        ((TextView) v).getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
         // Create a new user with a first and last name
