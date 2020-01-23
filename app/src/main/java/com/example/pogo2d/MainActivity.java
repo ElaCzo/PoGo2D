112e15f9cd76ba6a97f1cd1447a184e4939f501a
ElisabethAbbasZadeh <elisabeth.abbaszadeh@gmail.com>
2020-01-23 15:29:07 +0100

    Clean code main activity.

--- app/src/main/java/com/example/pogo2d/MainActivity.java
+++ app/src/main/java/com/example/pogo2d/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.pogo2d;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -27,14 +26,12 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity {
 
     /* EditTexts */
-    EditText pseudo;
     EditText email;
     EditText mdp;
     /* Buttons */
     Button jouer;
     Button OK;
     Button inscription;
-    private String TAG = "auth";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +39,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         /* EditTexts */
-        //pseudo = findViewById(R.id.pseudo);
         email = findViewById(R.id.email);
         mdp = findViewById(R.id.mdp);
 
@@ -52,12 +48,6 @@ public class MainActivity extends AppCompatActivity {
         inscription = findViewById(R.id.inscription);
 
         OK.setVisibility(View.GONE);
-        //email.setVisibility(View.GONE);
-
-
-        // regarder stocker auth firebase.
-        // recup ds autre act
-        // pattern singleton
 
         jouer.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -102,26 +92,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void createAccount(String email, String password) {
-        Log.d(TAG, "createAccount:" + email);
         if (!validateForm()) {
             return;
         }
-
-        Log.d(TAG, "1createUserWithEmail:success");
 
         FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        Log.d(TAG, "2createUserWithEmail:success");
 
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "createUserWithEmail:success");
                             final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                             // Add a new document with a generated ID
-
-                            Globals.setDB(FirebaseFirestore.getInstance());
 
                             Map<String, Object> data = new HashMap<>();
                             data.put("number", 0);
@@ -132,18 +115,15 @@ public class MainActivity extends AppCompatActivity {
                                     .addOnSuccessListener(new OnSuccessListener<Void>() {
                                         @Override
                                         public void onSuccess(Void aVoid) {
-                                            Log.i(TAG, "Success adding document");
                                         }
                                     })
                                     .addOnFailureListener(new OnFailureListener() {
                                         @Override
                                         public void onFailure(@NonNull Exception e) {
-                                            Log.w(TAG, "Error adding document", e);
                                         }
                                     });
                         } else {
                             // If sign in fails, display a message to the user.
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
                             Toast.makeText(MainActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
                         }
@@ -152,7 +132,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void signIn(String email, String password) {
-        Log.d(TAG, "signIn:" + email);
         if (!validateForm()) {
             return;
         }
@@ -162,15 +141,12 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithEmail:success");
-                            Globals.setMAuth(Globals.getInstance().getMAuth());
+                            // Sign in success, change activity
 
                             Intent intent = new Intent(MainActivity.this, CollectionActivity.class);
                             startActivity(intent);
                         } else {
                             // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithEmail:failure", task.getException());
                             Toast.makeText(MainActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
                         }
@@ -179,29 +155,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void signOut() {
-        Globals.getInstance().getMAuth().signOut();
     }
 
     private void sendEmailVerification() {
-        // Disable button
-        //findViewById(R.id.verifyEmailButton).setEnabled(false);
 
         // Send verification email
-        final FirebaseUser user = Globals.getInstance().getMAuth().getCurrentUser();
-        user.sendEmailVerification()
+        FirebaseAuth.getInstance().getCurrentUser().sendEmailVerification()
                 .addOnCompleteListener(this, new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
-                        // [START_EXCLUDE]
-                        // Re-enable button
-                        //findViewById(R.id.verifyEmailButton).setEnabled(true);
 
                         if (task.isSuccessful()) {
                             Toast.makeText(MainActivity.this,
-                                    "Verification email sent to " + user.getEmail(),
+                                    "Verification email sent to " +
+                                            FirebaseAuth.getInstance().getCurrentUser().getEmail(),
                                     Toast.LENGTH_SHORT).show();
                         } else {
-                            Log.e(TAG, "sendEmailVerification", task.getException());
                             Toast.makeText(MainActivity.this,
                                     "Failed to send verification email.",
                                     Toast.LENGTH_SHORT).show();
